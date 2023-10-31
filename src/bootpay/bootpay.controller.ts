a6db2455e2d12bd028444abdf6aa202a823cc558
nick <sonminsoon@gmail.com>
2023-10-31 11:26:06 +0900

    vBankPayment body

--- src/bootpay/bootpay.controller.ts
+++ src/bootpay/bootpay.controller.ts
@@ -7,8 +7,12 @@ export class BootpayController {
 
   @Post('vBankPayment')
   vBankPayment(@Body() body) {
-    console.log("-> vBankPayment")
-    return 'This action adds a new bootpay'
+    console.log("=>(bootpay.controller.ts:10) body", body);
+    //JSON Decode 체크
+    // 웹훅 서버에서 응답한 내용에서 JSON 파싱이 실패하였습니다. unexpected token at 'This action adds a new bootpay'
+
+
+    return {"success":true}
   }
 
   @Get('vBankPayment2')
