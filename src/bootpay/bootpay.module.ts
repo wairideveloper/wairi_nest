8e0eade14cef2084fc85248fdc1491104a10cb54
nick <sonminsoon@gmail.com>
2023-10-31 14:12:32 +0900

    vbank CampaignSubmit update

--- src/bootpay/bootpay.module.ts
+++ src/bootpay/bootpay.module.ts
@@ -3,14 +3,17 @@ import { BootpayService } from './bootpay.service';
 import { BootpayController } from './bootpay.controller';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {Payment} from "../../entity/entities/Payment";
+import {CampaignSubmit} from "../../entity/entities/CampaignSubmit";
+import {SubmitModelService} from "../graphql/submit_model/submit_model.service";
+import {CampaignItemSchedule} from "../../entity/entities/CampaignItemSchedule";
 
 @Module({
   imports: [
       TypeOrmModule.forFeature([
-          Payment
+          Payment, CampaignSubmit, CampaignSubmit, CampaignItemSchedule
         ]),
   ],
   controllers: [BootpayController],
-  providers: [BootpayService]
+  providers: [BootpayService, SubmitModelService]
 })
 export class BootpayModule {}
--- src/bootpay/bootpay.service.ts
+++ src/bootpay/bootpay.service.ts
@@ -1,19 +1,83 @@
-import { Injectable } from '@nestjs/common';
+import {HttpException, Injectable} from '@nestjs/common';
 import {InjectRepository} from "@nestjs/typeorm";
 import {Payment} from "../../entity/entities/Payment";
 import {Repository} from "typeorm";
 import {getNowUnix, getUnixTimeStampByDate} from "../util/common";
-
+import {Connection} from "typeorm";
+import {CampaignSubmit} from "../../entity/entities/CampaignSubmit";
+import {CampaignItemSchedule} from "../../entity/entities/CampaignItemSchedule";
+import {SubmitModelService} from "../graphql/submit_model/submit_model.service";
 @Injectable()
 export class BootpayService {
 
     constructor(
         @InjectRepository(Payment)
         private readonly paymentRepository: Repository<Payment>,
+        @InjectRepository(CampaignSubmit)
+        private readonly campaignSubmitRepository: Repository<CampaignSubmit>,
+        @InjectRepository(CampaignItemSchedule)
+        private readonly campaignItemScheduleRepository: Repository<CampaignItemSchedule>,
+        private connection: Connection,
+        private readonly submitModelService: SubmitModelService
+
     ) {}
     async updateVbankPayment(body) {
+        const queryRunner = this.connection.createQueryRunner();
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+
         try{
-            await this.paymentRepository.createQueryBuilder()
+            const payment = await queryRunner.manager.findOne(Payment, {
+                where: {
+                    oid: body.order_id
+                }
+            })
+            if(!payment) {
+                throw new Error("결제 정보가 존재하지 않습니다.")
+            }
+            const submit = await queryRunner.manager.findOne(CampaignSubmit, {
+                where: {
+                    idx: payment.submitIdx
+                }
+            })
+            if(!submit) {
+                throw new Error("신청 정보가 존재하지 않습니다.")
+            }
+
+            const campaignItemSchdule = await this.submitModelService.getCampaignItemSchduleByItemIdxAndRangeDate(
+                submit.itemIdx, submit.startDate, submit.endDate)
+
+            let itemSchduleIdx = [];
+            campaignItemSchdule.forEach((item) => {
+                // stock 확인 nop > stock
+                itemSchduleIdx.push(item.idx);
+                if(submit.nop > item.stock){
+                    throw new HttpException("재고가 부족합니다.", 404);
+                }
+            })
+
+            if(itemSchduleIdx.length > 0){
+                // update campaign_item_schedule.stock = campaign_item_schedule.stock - submit.nop
+                await queryRunner.manager.createQueryBuilder()
+                    .update(CampaignItemSchedule)
+                    .set({
+                        stock: () => `stock - ${submit.nop}`,
+                    })
+                    .where("idx IN (:...idx)", { idx: itemSchduleIdx })
+                    .execute();
+            }
+
+            // update submit.status = 300
+            await queryRunner.manager.createQueryBuilder()
+                .update(CampaignSubmit)
+                .set({
+                    status: 300,
+                    paymentIdx: payment.idx,
+                })
+                .where("idx = :idx", { idx: submit.idx })
+                .execute();
+
+            await queryRunner.manager.createQueryBuilder()
                 .update(Payment)
                 .set({
                     status: 200,
@@ -22,7 +86,9 @@ export class BootpayService {
                 .where("oid = :oid", { oid: body.order_id })
                 .andWhere("status = :status", { status: 100 })
                 .execute();
+            await queryRunner.commitTransaction();
         }catch (e) {
+            await queryRunner.rollbackTransaction();
             console.log(e)
         }
     }
