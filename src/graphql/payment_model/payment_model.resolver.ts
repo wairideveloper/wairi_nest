280c3265d12531ef8acd0bde7c95acc96bebb1ff
nick <sonminsoon@gmail.com>
2024-01-16 12:43:42 +0900

    이용일자

--- src/graphql/payment_model/payment_model.resolver.ts
+++ src/graphql/payment_model/payment_model.resolver.ts
@@ -1,7 +1,7 @@
 import {Args, Mutation, Query, Resolver} from '@nestjs/graphql';
 import {PaymentModelService} from './payment_model.service';
 import {SubmitModelService} from "../submit_model/submit_model.service";
-import {getAfter3Days, getBootpayStatusText} from "../../util/common";
+import {FROM_UNIXTIME_JS_YY_MM_DD, getAfter3Days, getBootpayStatusText} from "../../util/common";
 import {HttpException, UseGuards} from "@nestjs/common";
 import {GqlAuthGuard} from "../../auth/GqlAuthGuard";
 import {AuthUser} from "../../auth/auth-user.decorator";
@@ -91,8 +91,10 @@ export class PaymentModelResolver {
                         "가상계좌번호": response.vbank_data.bank_account,
                         "캠페인이름": campaign.name,
                         "업체이름": partner.corpName,
-                        "이용일자": submitItem.startDate + ' ~ ' + submitItem.endDate,
+                        //YYYY-MM-DD 이용일자
+                        "이용일자": FROM_UNIXTIME_JS_YY_MM_DD(submitItem.startDate) + ' ~ ' + FROM_UNIXTIME_JS_YY_MM_DD(submitItem.endDate),
                         "인원": submitItem.nop,
+                        "입금액": response.price,
                         "캠페인페이지승인링크": 'https://wairi.co.kr/extranet/campaign/submit#/' + submitItem.idx,
                     }
                     // @ts-ignore
