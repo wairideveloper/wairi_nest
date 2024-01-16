b99a75a92960ff671d641d92e4e6ea5040a902c6
nick <sonminsoon@gmail.com>
2024-01-16 12:38:12 +0900

    auth user name

--- src/graphql/payment_model/payment_model.resolver.ts
+++ src/graphql/payment_model/payment_model.resolver.ts
@@ -86,7 +86,7 @@ export class PaymentModelResolver {
                     const partner = await this.submitModelService.getPartnerByPartnerIdx(campaign.partnerIdx);
                     //Todo apiplex 가상계좌
                     let param = {
-                        "이름": authUser.name,
+                        "이름": authUser.username,
                         "가상계좌은행": response.vbank_data.bank_name,
                         "가상계좌번호": response.vbank_data.bank_account,
                         "캠페인이름": campaign.name,
