7100279259f663f578ce2ba0adc00d4e9bcb66cc
nick <sonminsoon@gmail.com>
2024-05-17 10:32:19 +0900

    update payment check

--- src/bootpay/bootpay.service.ts
+++ src/bootpay/bootpay.service.ts
@@ -5,6 +5,7 @@ import {Repository} from "typeorm";
 import {getNowUnix, getUnixTimeStampByDate} from "../util/common";
 import {Connection} from "typeorm";
 import {CampaignSubmit} from "../../entity/entities/CampaignSubmit";
+import {CampaignItem} from "../../entity/entities/CampaignItem";
 import {CampaignItemSchedule} from "../../entity/entities/CampaignItemSchedule";
 import {SubmitModelService} from "../graphql/submit_model/submit_model.service";
 @Injectable()
@@ -17,6 +18,8 @@ export class BootpayService {
         private readonly campaignSubmitRepository: Repository<CampaignSubmit>,
         @InjectRepository(CampaignItemSchedule)
         private readonly campaignItemScheduleRepository: Repository<CampaignItemSchedule>,
+        @InjectRepository(CampaignItem)
+        private readonly campaignItemRepository: Repository<CampaignItem>,
         private connection: Connection,
         private readonly submitModelService: SubmitModelService
 
@@ -86,6 +89,85 @@ export class BootpayService {
                 .execute();
             await queryRunner.commitTransaction();
         }catch (e) {
+            await queryRunner.rollbackTransaction();
+            console.log(e)
+        }finally {
+            await queryRunner.release();
+        }
+    }
+
+    async updatePayment(body:any) {
+        if(body.status != 1){
+            throw new HttpException("status is not 1", 404);
+        }
+        const queryRunner = this.connection.createQueryRunner();
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+        try{
+            const payment = await queryRunner.manager.findOne(Payment, {
+                where: {
+                    oid: body.order_id
+                }
+            })
+            if(!payment) {
+                throw new Error("결제 정보가 존재하지 않습니다.")
+            }
+            const submit = await queryRunner.manager.findOne(CampaignSubmit, {
+                where: {
+                    idx: payment.submitIdx
+                }
+            })
+            if(!submit) {
+                throw new Error("신청 정보가 존재하지 않습니다.")
+            }
+
+            const camppaignItem = await queryRunner.manager.findOne(CampaignItem, {
+                where: {
+                    idx: submit.itemIdx
+                }
+            })
+
+            let count: any;
+            if(camppaignItem.calcType1 == 1){
+                count = submit.nights;
+            }else{
+                count = submit.nop;
+            }
+
+            const campaignItemSchdule = await this.submitModelService.getCampaignItemSchduleByItemIdxAndRangeDate(
+                submit.itemIdx, submit.startDate, submit.endDate)
+
+            let itemSchduleIdx = [];
+            campaignItemSchdule.forEach((item) => {
+                // stock 확인 nop > stock
+                itemSchduleIdx.push(item.idx);
+                if(count > item.stock){
+                    throw new HttpException("재고가 부족합니다.", 404);
+                }
+            })
+
+            // update submit.status = 300
+            await queryRunner.manager.createQueryBuilder()
+                .update(CampaignSubmit)
+                .set({
+                    status: 300,
+                    paymentIdx: payment.idx,
+                })
+                .where("idx = :idx", { idx: submit.idx })
+                .execute();
+
+            await queryRunner.manager.createQueryBuilder()
+                .update(Payment)
+                .set({
+                    status: 200,
+                    paydate: getUnixTimeStampByDate(body.purchased_at),
+                })
+                .where("oid = :oid", { oid: body.order_id })
+                .andWhere("status = :status", { status: 100 })
+                .execute();
+            await queryRunner.commitTransaction();
+
+        }catch (e){
             await queryRunner.rollbackTransaction();
             console.log(e)
         }finally {
